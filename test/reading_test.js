2a35aa79a5fdce52a4f50fb9d2b64c4cef55ff67
boyang1020 <boyang1020@gmail.com>
2018-10-02 15:40:21 -0700

    the id property

--- test/reading_test.js
+++ test/reading_test.js
@@ -4,13 +4,17 @@ const User = require('../src/user');
 describe('Reading users out of the database', () => {
     let joe;
 
-    beforeEach(() => {
-        joe = new User({ name: 'Joe'});
+    beforeEach((done) => {
+        joe = new User({ name: 'Joe' });
         joe.save()
-         .then(() => done());
+            .then(() => done());
     });
 
-    it('finds all users with a name of joe', () => {
-        
+    it('finds all users with a name of joe', (done) => {
+        User.find({ name: 'Joe' })
+            .then((users) => {
+                assert(users[0]._id.toString() === joe._id.toString());
+                done();
+            });
     });
 });
