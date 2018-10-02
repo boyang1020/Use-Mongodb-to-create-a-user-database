571ded66f7b3373f82252c5f7f62f2fa85481886
boyang1020 <boyang1020@gmail.com>
2018-10-02 11:01:44 -0700

    default promise implemetation

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,15 +1,21 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/users_test',  { useNewUrlParser: true });
-mongoose.connection
-    .once('open', () => console.log('Good to go!'))
-    .on('error', (error) => {
-        console.warn('Warning', error);
-    });
+mongoose.Promise = global.Promise;
+
+before((done) => {
+    mongoose.connect('mongodb://localhost/users_test', { useNewUrlParser: true });
+    mongoose.connection
+        .once('open', () => { done(); })
+        .on('error', (error) => {
+            console.warn('Warning', error);
+        });
+});
+
+
 
 beforeEach((done) => {
-        mongoose.connection.collections.users.drop(() => {
-            // Ready to run the next test!
-            done();
-        });
+    mongoose.connection.collections.users.drop(() => {
+        // Ready to run the next test!
+        done();
     });
+});
